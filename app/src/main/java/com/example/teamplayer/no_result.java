fa64056e3cac0f1a86bdee638202a49e04ac1f95
teamPlayer-project <60099970+teamPlayer-project@users.noreply.github.com>
2020-03-09 20:43:55 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/teamplayer/no_result.java
@@ -0,0 +1,14 @@
+package com.example.teamplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class no_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_result);
+    }
+}
